50db1ee5750806c4fb85d8682d4262b1823036c4
Arnab Chakraborty <iam-arnab@outlook.com>
2025-04-11 14:00:11 +0530

    Add low battery notification alert and code formatting improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,41 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
 #include "findLabelValue.h"
+#include <chrono>
+#include <iostream>
+#include <thread>
 
-int main()
-{
-    while (true)
-    {
-        std::string battery_state, battery_percentage;
-        const std::string BATTERY_INFO = "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
+int main() {
+  while (true) {
+    std::string battery_state, battery_percentage;
+    const std::string BATTERY_INFO =
+        "upower -i /org/freedesktop/UPower/devices/battery_BAT1";
 
-        FILE* pipe = popen(BATTERY_INFO.c_str(), "r");
-        if (!pipe)
-        {
-            std::cout << "Command failed to execute";
-            return 1;
-        }
-
-        char buffer[128];
-        std::string str = "";
-        while (fgets(buffer, sizeof(buffer), pipe) != NULL)
-        {
-            str += std::string(buffer);
-        }
-
-        pclose(pipe);
-
-        battery_state = findLabelValue(str, "state:");
-        battery_percentage = findLabelValue(str, "percentage:");
-        
-        if (battery_state == "charging" && battery_percentage >= "85%")
-        {
-            system("notify-send 'Battery Reached 85'");
-            system("paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga");
-        }
-        
-        std::this_thread::sleep_for(std::chrono::seconds(120));
+    FILE *pipe = popen(BATTERY_INFO.c_str(), "r");
+    if (!pipe) {
+      std::cout << "Command failed to execute";
+      return 1;
     }
+
+    char buffer[128];
+    std::string str = "";
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+      str += std::string(buffer);
+    }
+
+    pclose(pipe);
+
+    battery_state = findLabelValue(str, "state:");
+    battery_percentage = findLabelValue(str, "percentage:");
+
+    if (battery_state == "charging" && battery_percentage >= "85%") {
+      system("notify-send 'Battery Reached 85'");
+      system("paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga");
+    }
+
+    if (battery_state == "discharging" && battery_percentage <= "25%") {
+      system("notify-send 'Battery Reached 25'");
+      system("paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga");
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(120));
+  }
 }
